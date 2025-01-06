0b6d31b9ad513cb32a8f6014b97047dd4b5e04d9
chris cerne <github@corp.ccerne.com>
2025-01-05 19:29:54 -0500

    Force LF line endings

--- src/decompiler/function_decompiler.rs
+++ src/decompiler/function_decompiler.rs
@@ -47,11 +47,11 @@ pub enum FunctionDecompilerError {
     UnimplementedOpcode(Opcode, BasicBlockId),
 }
 
-/// TODO: Map instructions to a reference value (for usage with loop variables, etc.)
-/// TODO: We should call loop variables instruction references (InstrRef)
-/// TODO: We should have an AST pass that identifies variables with identifiers that are
-/// TODO: the same, and wrap them in an InstrRef (for MemberAccess & Identifier) since
-/// TODO: this will help further analysis
+// TODO: Map instructions to a reference value (for usage with loop variables, etc.)
+// TODO: We should call loop variables instruction references (InstrRef)
+// TODO: We should have an AST pass that identifies variables with identifiers that are
+// TODO: the same, and wrap them in an InstrRef (for MemberAccess & Identifier) since
+// TODO: this will help further analysis
 
 /// A struct to hold the state of a function decompiler
 pub struct FunctionDecompiler {
