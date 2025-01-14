fd0c833698718d8ebfd4fc1e0617ba97962fa88a
chris cerne <github@corp.ccerne.com>
2025-01-14 12:06:53 -0500

    True, false do not have an operand

--- gbf_core/src/decompiler/handlers/literal.rs
+++ gbf_core/src/decompiler/handlers/literal.rs
@@ -23,30 +23,34 @@ impl OpcodeHandler for LiteralHandler {
         context: &mut FunctionDecompilerContext,
         instruction: &Instruction,
     ) -> Result<ProcessedInstruction, FunctionDecompilerError> {
-        // For literals, the opcode must contain the literal value as an operand.
-        let operand = instruction.operand.as_ref().ok_or(
-            FunctionDecompilerError::InstructionMustHaveOperand(instruction.opcode),
-        )?;
+        // For PushTrue, PushFalse, we do not pop an operand
+        let literal =
+            if instruction.opcode == Opcode::PushTrue || instruction.opcode == Opcode::PushFalse {
+                new_bool(instruction.opcode == Opcode::PushTrue)
+            } else {
+                // For literals, the opcode must contain the literal value as an operand.
+                let operand = instruction.operand.as_ref().ok_or(
+                    FunctionDecompilerError::InstructionMustHaveOperand(instruction.opcode),
+                )?;
 
-        let literal = match instruction.opcode {
-            Opcode::PushString => new_str(operand.get_string_value()?),
-            Opcode::PushNumber => match operand {
-                Operand::Float(_) => new_float(operand.get_string_value()?),
-                Operand::Number(_) => new_num(operand.get_number_value()?),
-                _ => {
-                    // We should not hit this case ever.
-                    panic!("Invalid operand type for PushNumber opcode: {:?}", operand);
+                match instruction.opcode {
+                    Opcode::PushString => new_str(operand.get_string_value()?),
+                    Opcode::PushNumber => match operand {
+                        Operand::Float(_) => new_float(operand.get_string_value()?),
+                        Operand::Number(_) => new_num(operand.get_number_value()?),
+                        _ => {
+                            // We should not hit this case ever.
+                            panic!("Invalid operand type for PushNumber opcode: {:?}", operand);
+                        }
+                    },
+                    _ => {
+                        return Err(FunctionDecompilerError::UnimplementedOpcode(
+                            instruction.opcode,
+                            context.current_block_id.unwrap(),
+                        ));
+                    }
                 }
-            },
-            Opcode::PushTrue => new_bool(true),
-            Opcode::PushFalse => new_bool(false),
-            _ => {
-                return Err(FunctionDecompilerError::UnimplementedOpcode(
-                    instruction.opcode,
-                    context.current_block_id.unwrap(),
-                ));
-            }
-        };
+            };
 
         // let ver = context.ssa_context.new_ssa_version_for("lit");
         // let ssa_id = new_id_with_version("lit", ver);
